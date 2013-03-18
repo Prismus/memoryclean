925f69953ce2d266020dcd3532a9f6ee2db27d57
Georges <gku@itgk.ch>
2013-03-18 13:21:27 +0100

    Update cleanmemory.sh

--- cleanmemory.sh
+++ cleanmemory.sh
@@ -4,6 +4,7 @@
 ### Clear unused memory                         ###
 ###     Auteur: Georges Kut                     ###
 ###     email: gku@itgk.ch                      ###
+###     For Ubuntu Linux-Mint Debian            ###
 ###################################################
 
 echo "Clear Unused Memory Start"
@@ -12,6 +13,6 @@ echo "1" > sysctl -w vm.drop_caches=3
 
 sleep 5
 
-echo "0" > sync && echo 3 | sudo tee /proc/sys/vm/drop_caches
+echo "0" >  sudo sync && echo 3 | sudo tee /proc/sys/vm/drop_caches
 
 echo "Cleaning unsed memory Finish"
