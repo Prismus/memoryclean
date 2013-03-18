f9dd2eb8f235c344eeb10fdbadec521dd4d49758
Georges Kut <gku@itgk.ch>
2013-03-18 13:23:32 +0100

    V1

---
+++ cleanmemoryother.sh
@@ -0,0 +1,18 @@
+﻿#!/bin/bash
+
+###################################################
+###         Clear unused memory                        			   ###
+###         Auteur: Georges Kut                     				   ###
+###         email: gku@itgk.ch                     					   ###
+###         For Redhat Suse Fedora                                 ### 
+###################################################
+
+echo "Clear Unused Memory Start"
+
+echo "1" > sysctl -w vm.drop_caches=3
+
+sleep 5
+
+echo "0" > sync && echo 3 | tee /proc/sys/vm/drop_caches
+
+echo "Cleaning unsed memory Finish"
